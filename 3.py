043fac4a58e698ab1111a94dcb0df2659be4958a
gaamiranda <goncalo.andre.miranda@gmail.com>
2025-01-02 15:15:24 +0000

    melhoração 3.py

--- 3.py
+++ 3.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import os
 
 # Certifique-se de instalar o pacote correto: opencv-contrib-python
 # pip install opencv-contrib-python
@@ -7,17 +8,28 @@ import numpy as np
 # Inicializa o reconhecedor LBPH
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 
-# Exemplo de imagens de treino e seus rótulos
-training_images = [
-    cv2.imread("imgs/gm_feliz.jpg", cv2.IMREAD_GRAYSCALE),
-    cv2.imread("imgs/gm_neutro.jpg", cv2.IMREAD_GRAYSCALE)
-]
+# # Exemplo de imagens de treino e seus rótulos
+# training_images = [
+#     cv2.imread("imgs/gm_feliz.jpg", cv2.IMREAD_GRAYSCALE),
+#     cv2.imread("imgs/gm_neutro.jpg", cv2.IMREAD_GRAYSCALE)
+# ]
+
+imgs_directory = "imgs/"
+training_images = []
+labels = []
+
+for idx, filename in enumerate(os.listdir(imgs_directory)):
+    if filename.endswith(('.jpg', '.jpeg', '.png')):
+        img_path = os.path.join(imgs_directory, filename)
+        image = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+        if image is not None:
+            training_images.append(image)
+            labels.append(idx)
 
 if not training_images:
 	print("Error loading training_images")
 	exit()
 
-labels = [1, 2]  # Rótulos correspondentes às imagens de treinamento
 if not labels or len(training_images) != len(labels):
     print("Error: Labels are missing or do not match the number of training images.")
     exit()
@@ -32,8 +44,9 @@ labels = np.array(labels)
 # Treina o modelo
 recognizer.train(training_images, labels)
 
-# Carrega a imagem de teste e a prepara
-test_image = cv2.imread("imgs/gm_neutro.jpg", cv2.IMREAD_GRAYSCALE)
+# Carrega a imagem de teste
+#Não esquecer que a imagem não pode estar na directoria imgs se quisermos um alto grau de confiança
+test_image = cv2.imread("gui1.jpeg", cv2.IMREAD_GRAYSCALE)
 test_image = cv2.resize(test_image, (100, 100))
 
 # Prediz o rótulo e a confiança para a imagem de teste
